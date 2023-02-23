e27eb453eb3835a451e5febac8a4033fc741fc3d
Romaric250 <lonfonyuyromeo@gmail.com>
2023-02-22 17:46:51 -0800

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * @c: An input character
+ * _islower: An input character
  * Description:function prints alpahabet in lowercase
  * Return: 1 if is lowercase or 0 if is uppercase
  */
 int _islower(int c)
 {
-	char i;
-	int lower = 0;
+        char i;
+        int lower = 0;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i == c)
-			lower = 1;
-	}
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                if (i == c)
+                        lower = 1;
+        }
 
-	return (lower);
+        return (lower);
 }
-
-
