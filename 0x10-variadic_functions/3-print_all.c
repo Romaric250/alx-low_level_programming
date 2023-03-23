30004123d2cae8a0df4615e3c21db61791b5b671
Romaric250 <lonfonyuyromeo@gmail.com>
2023-03-23 14:13:31 -0700

    task 0,1,2,3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything it touches 🤷‍♂️🤷‍♂️🙌
+ * @format: A list of type of arguments passed to the function.
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *rem;
+	int i = 0;
+
+	va_start(ap, format);
+	while (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", (char) va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(ap, double));
+				break;
+			case 's':
+				rem = va_arg(ap, char*);
+				if (rem != NULL)
+				{
+					printf("%s", rem);
+					break;
+				}
+				printf("(nil)");
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+					format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
