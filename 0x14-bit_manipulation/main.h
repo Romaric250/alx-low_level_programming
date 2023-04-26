6944e226df7246edb9cc02391ccc3a701084b49e
Romaric250 <lonfonyuyromeo@gmail.com>
2023-04-26 03:21:11 -0700

    task 2 corretet

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,12 @@
 #ifndef BITS_H
 #define BITS_H
-
+#include<stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
-
+void print_binary(unsigned long int n);
 
 
 
