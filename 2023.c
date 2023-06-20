615305312038713635aa16b53f31f4625a2d0dd2
Romaric250 <lonfonyuyromeo@gmail.com>
2023-06-20 09:26:04 -0700

    2023 gce csc

---
+++ 2023.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int OutputLeadingSpaces(int nSpaces){
+        int i;
+        for (i = 0; i <= nSpaces; i++)
+        {
+                printf(" ");
+        }
+}
+int OutputLineOfStars(int nStars)
+{
+        int i;
+        for (i = 0; i <= nStars; i++)
+        {
+                printf("*");
+        }
+        printf("\n");
+}
+ 
+void Adjust(int NoOfSpaces, int NoOfStars)
+{
+        OutputLeadingSpaces(NoOfSpaces);
+        OutputLineOfStars(NoOfStars);
+}
+
+void Initialise(int MaxNoOfStars, int NoOfSpaces, int NoOfStars)
+{
+        int i, j;
+        for (i = 0; i < MaxNoOfStars; i++)
+        {
+                Adjust(NoOfSpaces, NoOfStars);
+                if (i < MaxNoOfStars/2)
+                {
+                        NoOfSpaces--;
+                        NoOfStars += 2;
+                }
+                else
+                {
+                        NoOfSpaces++;
+                        NoOfStars -= 2;
+                }
+        }
+}
+
+
+
+
+
+int main()
+{
+	int MaxNoOfStars = 9;
+	int NoOfSpaces = MaxNoOfStars / 2;
+	int NoOfStars = 1;
+
+	Initialise(MaxNoOfStars, NoOfSpaces, NoOfStars);
+
+	return(0);
+}
+
